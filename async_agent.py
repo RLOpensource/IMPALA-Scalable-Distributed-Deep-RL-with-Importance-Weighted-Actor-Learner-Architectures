7048f6d962ddb3238fe61e1948fb81815f8e87b5
차금강 <chageumgang@chageumgang-ui-MacBookPro.local>
2019-06-14 13:07:00 +0900

    add

--- async_agent.py
+++ async_agent.py
@@ -43,69 +43,66 @@ class Agent(threading.Thread):
         total_max_prob = 0
         loss_step = 0
 
-        episode_state = []
-        episode_next_state = []
-        episode_reward = []
-        episode_done = []
-        episode_action = []
-        episode_behavior_policy = []
-
         writer = tensorboardX.SummaryWriter('runs/'+self.name)
 
         while True:
-            action, behavior_policy, max_prob = self.local_network.get_policy_and_action(state)
-            episode_step += 1
-            total_max_prob += max_prob
+            loss_step += 1
+            episode_state = []
+            episode_next_state = []
+            episode_reward = []
+            episode_done = []
+            episode_action = []
+            episode_behavior_policy = []
+            for i in range(128):
+
+                action, behavior_policy, max_prob = self.local_network.get_policy_and_action(state)
+                episode_step += 1
+                total_max_prob += max_prob
             
-            next_state, reward, done, _ = self.env.step(action)
+                next_state, reward, done, _ = self.env.step(action)
 
-            score += reward
+                score += reward
 
-            reward = 0
-            if done:
-                if score == 500:
-                    reward = 0
-                else:
-                    reward = -1
+                reward = 0
+                if done:
+                    if score == 500:
+                        reward = 1
+                    else:
+                        reward = -1
 
-            episode_state.append(state)
-            episode_next_state.append(next_state)
-            episode_reward.append(reward)
-            episode_done.append(done)
-            episode_action.append(action)
-            episode_behavior_policy.append(behavior_policy)
+                episode_state.append(state)
+                episode_next_state.append(next_state)
+                episode_reward.append(reward)
+                episode_done.append(done)
+                episode_action.append(action)
+                episode_behavior_policy.append(behavior_policy)
 
-            state = next_state
+                state = next_state
 
-            if done:
-                lock.acquire()
-                pi_loss, value_loss, entropy = self.global_network.train(
-                    state=np.stack(episode_state),
-                    next_state=np.stack(episode_next_state),
-                    reward=np.stack(episode_reward),
-                    done=np.stack(episode_done),
-                    action=np.stack(episode_action),
-                    behavior_policy=np.stack(episode_behavior_policy))
-                self.sess.run(self.global_to_local)
-                lock.release()
-                episode_state = []
-                episode_next_state = []
-                episode_reward = []
-                episode_done = []
-                episode_action = []
-                episode_behavior_policy = []
-                writer.add_scalar('pi_loss', pi_loss, episode)
-                writer.add_scalar('value_loss', value_loss, episode)
-                writer.add_scalar('entropy', entropy, episode)
-                writer.add_scalar('score', score, episode)
-                writer.add_scalar('max_prob', total_max_prob / episode_step, episode)
-                print(self.name, episode, score, total_max_prob / episode_step)
-                episode_step = 0
-                total_max_prob = 0
-                episode += 1
-                score  = 0
-                done = False
-                state = self.env.reset()
+                if done:
+                    print(self.name, episode, score, total_max_prob / episode_step)
+                    writer.add_scalar('score', score, episode)
+                    writer.add_scalar('max_prob', total_max_prob / episode_step, episode)
+                    episode_step = 0
+                    total_max_prob = 0
+                    episode += 1
+                    score  = 0
+                    done = False
+                    state = self.env.reset()
+
+            lock.acquire()
+            pi_loss, value_loss, entropy = self.global_network.train(
+                state=np.stack(episode_state),
+                next_state=np.stack(episode_next_state),
+                reward=np.stack(episode_reward),
+                done=np.stack(episode_done),
+                action=np.stack(episode_action),
+                behavior_policy=np.stack(episode_behavior_policy))
+            self.sess.run(self.global_to_local)
+            writer.add_scalar('pi_loss', pi_loss, loss_step)
+            writer.add_scalar('value_loss', value_loss, loss_step)
+            writer.add_scalar('entropy', entropy, loss_step)
+            lock.release()
                 
 
 if __name__ == '__main__':
